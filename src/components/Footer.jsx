215ed835774dc91f534fd7c91c4b363dd4be45c9
phihungdeptraino2 <hungdz1372004@gmail.com>
2025-04-19 09:29:07 +0700

    Con phan Edittor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,24 +14,36 @@ const Footer = () => {
             <input type="email" placeholder="Enter your email" />
             <button>Send</button>
           </div>
+          <div className="footer-bottom">
+            <div className="footer-logo">
+              <img src="/image/avatar_small.png" alt="Chefify Logo" />
+            </div>
+            <p>2023 Chefify Company</p>
+            <p>Terms of Service | Privacy Policy</p>
+          </div>
         </div>
         <div className="footer-links">
-          <div>
-            <h3>Learn More</h3>
-            <ul>
-              <li>Our Cooks</li>
-              <li>See Our Features</li>
-              <li>FAQ</li>
-            </ul>
+          <div
+            className="
+          section1"
+          >
+            <div>
+              <h3>Learn More</h3>
+              <ul>
+                <li>Our Cooks</li>
+                <li>See Our Features</li>
+                <li>FAQ</li>
+              </ul>
+            </div>
+            <div>
+              <h3>Shop</h3>
+              <ul>
+                <li>Gift Subscription</li>
+                <li>Send Us Feedback</li>
+              </ul>
+            </div>
           </div>
-          <div>
-            <h3>Shop</h3>
-            <ul>
-              <li>Gift Subscription</li>
-              <li>Send Us Feedback</li>
-            </ul>
-          </div>
-          <div>
+          <div className="section2">
             <h3>Recipes</h3>
             <ul>
               <li>What to Cook This Week</li>
@@ -44,14 +56,6 @@ const Footer = () => {
             </ul>
           </div>
         </div>
-      </div>
-      <div className="footer-bottom">
-        <div className="footer-logo">
-          <img src="/logo.png" alt="Chefify Logo" />
-          <span>Chefify</span>
-        </div>
-        <p>2023 Chefify Company</p>
-        <p>Terms of Service | Privacy Policy</p>
       </div>
     </footer>
   );
